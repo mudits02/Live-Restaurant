56c78ee9eb3d86dbbdc1d57a20639a7b9217710e
mudits02 <mudits02@gmail.com>
2024-07-27 02:03:30 +0530

    Basics completed , used React using CDns , replaced in next push

--- App.js
+++ App.js
@@ -1,5 +1,42 @@
-    const heading = React.createElement("h1" , {id:"heading"} , "Hello World from React");
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement(
+    "div", 
+    { id: "parent" }, 
 
-    root.render(heading);
+    [React.createElement(
+        "div",
+        { id: "child" },
+
+        [React.createElement(
+            "h1",
+            {},
+            "Hello World from React nested divs h1"
+        ) , 
+        React.createElement(
+            "h2",
+            {},
+            "Hello World from React nested divs h2"
+        )]
+    ), 
+    React.createElement(
+        "div",
+        { id: "child2" },
+
+        [React.createElement(
+            "h1",
+            {},
+            "Hello World from React nested divs h1"
+        ) , 
+        React.createElement(
+            "h2",
+            {},
+            "Hello World from React nested divs h2"
+        )]
+    )]
+);
+
+//const heading = React.createElement("h1" , {id:"heading"} , "Hello World from React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
