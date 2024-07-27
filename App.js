ad893c1445438d5d20cfa8382e1b1dd31c592ab7
mudits02 <mudits02@gmail.com>
2024-07-28 01:19:39 +0530

    Finished Intro of Functional Components

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = (
-    <h1 className="head" >
-        Hello World using JSX
-    </h1>);
 
+const TitleHeading = () => (
+    <div id="title-container">
+        <h2 className="title">Hello</h2>
+    </div>
+);
 
+const HeaderComponent = () => {
+    return(
+        <div id="container">
+            <TitleHeading/>
+            <h1>Hello World from functional Component</h1>
+        </div>
+)}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeaderComponent />);
