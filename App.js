11b99d49af9fbbe3067d19f7f433369c2372c73b
mudits02 <mudits02@gmail.com>
2024-07-28 00:16:12 +0530

    JSX and Babel intro finished

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const heading = (
+    <h1 className="head" >
+        Hello World using JSX
+    </h1>);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
