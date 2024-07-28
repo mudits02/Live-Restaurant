16b9130e50b434888acabc6da1b8be39c39396e2
mudits02 <mudits02@gmail.com>
2024-07-29 01:30:36 +0530

    Done till props , code not working , bugged , will reslve in next push

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import logo from "./images/download.png"
 
 
-const TitleHeading = () => (
-    <div id="title-container">
-        <h2 className="title">Hello</h2>
-    </div>
-);
+const styleCard = {
+    backgroundColor : "#f0f0f0",
+}
 
-const HeaderComponent = () => {
+const RestaurantCard = (props) => {
+    console.log(props);
+    const {resData} = props;
     return(
-        <div id="container">
-            <TitleHeading/>
-            <h1>Hello World from functional Component</h1>
+        <div className="res-card" style={styleCard}>
+            <img 
+                className="res-logo"
+                alt="logo"
+                src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + 
+                    resData.data.cloudinaryImageId}
+            />
+            <h3>{resData.data.resName}</h3>
+            <h4>{resData.data.cuisines.join(", ")}</h4>
+            <h4>{resData.data.deliveryTime}</h4>
+            <h4>{resData.data.rating}</h4>
         </div>
-)}
+    )
+}
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={logo} alt="restaurant_logo" /> 
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resData = {resObj}/>
+                
+            </div>
+        </div>
+    )
+}
+
+const Applayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent />);
+root.render(<Applayout />);
