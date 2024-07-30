b84390e706b67fc3ca956137147f2d0d1bc62ab6
mudits02 <mudits02@gmail.com>
2024-07-30 22:54:22 +0530

    Clean Folder Structure

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import logo from "../../images/download.png"
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={logo} alt="restaurant_logo" /> 
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,29 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+    console.log(props);
+    const {resData} = props;
+
+    //Destructuring of Objects in Javascript , ? is optional chaining
+    const {name , cuisines , deliveryTime , lastMileTravel , cloudinaryImageId} = resData?.data;
+
+    const styleCard = {
+        backgroundColor : "#f0f0f0",
+    }
+
+    return(
+        <div className="res-card" style={styleCard}>
+            <img 
+                className="res-logo"
+                alt="logo"
+                src={CDN_URL + 
+                    cloudinaryImageId}
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{deliveryTime}</h4>
+        </div>
+    )
+}
+
+export default RestaurantCard;
